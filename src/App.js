02ef96348c6e3bfc0efe4da4e184bca385feda6a
fikymar <marcela.jaklova@gmail.com>
2021-08-17 22:21:25 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import NewArticle from "./components/NewArticle";
 import "./scss/App.css";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts";
+export const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
 	const [post, setPost] = useState(null);
 	const [articleId, setArticleId] = useState(1);
-
-	const [newArticle, setNewArticle] = useState(1);
-	const [title, setTitle] = useState("Your title");
-	const [newArticleBody, setNewArticleBody] = useState("Your text");
 
 	useEffect(() => {
 		axios.get(`${baseURL}/${articleId}`).then((res) => {
 			setPost(res.data);
 		});
 	}, [articleId]);
-
-	function handleChange(e) {
-		setTitle(e.target.value);
-	}
-
-	function handleChangeBody(e) {
-		setNewArticleBody(e.target.value);
-	}
-
-	function createPost() {
-		axios
-			.post(baseURL, {
-				id: "101",
-				title: title.toUpperCase(),
-				body: newArticleBody,
-			})
-			.then((res) => {
-				setNewArticle(res.data);
-			});
-	}
 
 	if (!post) return "No post!";
 
@@ -59,29 +36,8 @@ function App() {
 					<p>{post.body}</p>
 				</article>
 			</section>
-			<section className="createArticle">
-				<h1>Post your article</h1>
-				<label htmlFor="title">Enter title of your article: </label>
-				<input
-					type="text"
-					name="title"
-					onChange={handleChange}
-					placeholder={title}
-				/>
-				<label htmlFor="body">Enter your text: </label>
-				<textarea
-					type="text"
-					name="body"
-					onChange={handleChangeBody}
-					placeholder={newArticleBody}
-				/>
-				<button onClick={createPost}>Create Post</button>
-				<article>
-					<p className="articleNr"> {newArticle.id}</p>
-					<h2>{newArticle.title}</h2>
-					<p>{newArticle.body}</p>
-				</article>
-			</section>
+
+			<NewArticle />
 		</main>
 	);
 }
---
+++ src/components/NewArticle.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { baseURL } from "../App";
+
+const NewArticle = () => {
+	const [newArticle, setNewArticle] = useState(1);
+	const [title, setTitle] = useState("Your title");
+	const [newArticleBody, setNewArticleBody] = useState("Your text");
+
+	function createPost() {
+		axios
+			.post(baseURL, {
+				id: "101",
+				title: title.toUpperCase(),
+				body: newArticleBody,
+			})
+			.then((res) => {
+				setNewArticle(res.data);
+			});
+	}
+
+	function handleChange(e) {
+		setTitle(e.target.value);
+	}
+
+	function handleChangeBody(e) {
+		setNewArticleBody(e.target.value);
+	}
+
+	return (
+		<section className="createArticle">
+			<h1>Post your article</h1>
+			<label htmlFor="title">Enter title of your article: </label>
+			<input
+				type="text"
+				name="title"
+				onChange={handleChange}
+				placeholder={title}
+			/>
+			<label htmlFor="body">Enter your text: </label>
+			<textarea
+				type="text"
+				name="body"
+				onChange={handleChangeBody}
+				placeholder={newArticleBody}
+			/>
+			<button onClick={createPost}>Create Post</button>
+			<article>
+				<p className="articleNr"> {newArticle.id}</p>
+				<h2>{newArticle.title}</h2>
+				<p>{newArticle.body}</p>
+			</article>
+		</section>
+	);
+};
+
+export default NewArticle;
