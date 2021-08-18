0b538c7662b307d4f5de87950575d44aac5d560f
fikymar <marcela.jaklova@gmail.com>
2021-08-18 11:46:23 +0200

    add nextBTN

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ArticleById from "./components/ArticleById";
@@ -10,15 +10,20 @@ export const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
 	const [posts, setPosts] = useState([]);
+	const [showArticles, setShowArticles] = useState(1);
 
 	useEffect(() => {
-		axios.get(`${baseURL}?_limit=4`).then((res) => {
+		axios.get(`${baseURL}`).then((res) => {
 			setPosts(res.data);
 			console.log(posts);
 		});
 	}, []);
 
 	if (!posts) return "No post!";
+
+	function handleClick() {
+		setShowArticles(showArticles + 4);
+	}
 
 	return (
 		<div className="App">
@@ -30,13 +35,19 @@ function App() {
 						<h1>Articles</h1>
 						<Route path="/articles">
 							<h2>Load articles</h2>
-							{posts.map((post) => (
-								<article>
-									<p className="articleNr"> Article nr. {post.id}</p>
-									<h3>{post.title}</h3>
-									<p>{post.body}</p>
-								</article>
-							))}
+							{posts
+								.filter(
+									(post) =>
+										post.id < showArticles + 4 && post.id >= showArticles
+								)
+								.map((post) => (
+									<article key={post.id}>
+										<p className="articleNr"> Article nr. {post.id}</p>
+										<h3>{post.title}</h3>
+										<p>{post.body}</p>
+									</article>
+								))}
+							<button onClick={handleClick}>Next 4 articles</button>
 						</Route>
 						<Route path="/id">
 							<ArticleById />
